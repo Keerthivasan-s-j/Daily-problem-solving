b3d2995d2286e0bd7fb4d159140ff932cb27ffde
Keerthivasan-s-j <jagankeerthisri@gmail.com>
2025-05-16 20:57:38 +0530

    3340

---
+++ Leetcode/3340.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isBalanced(String num) {
+        int sum = 0;
+        for (int i = 0; i<num.length(); i++) {
+            if (i % 2 == 0){
+                sum += num.charAt(i) - '0';
+                continue;
+            }
+            sum -= num.charAt(i) - '0';
+        }
+        return sum == 0;
+    }
+}
