eb5ea6ee12c24561d25b13d7a22c14a6e2cc71a6
Keerthivasan-s-j <jagankeerthisri@gmail.com>
2025-05-17 13:43:26 +0530

    20

---
+++ Leetcode/20.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            if (ch == ')' && !stack.isEmpty() && stack.peek() == '('){
+                stack.pop();
+                continue;
+            }
+            if (ch == ']' && !stack.isEmpty() && stack.peek() == '['){
+                stack.pop();
+                continue;
+            }
+            if (ch == '}' && !stack.isEmpty() && stack.peek() == '{'){
+                stack.pop();
+                continue;
+            }
+            stack.push(ch);
+        }
+        return stack.isEmpty();
+    }
+}
