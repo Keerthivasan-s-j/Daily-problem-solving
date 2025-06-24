3015c947fa438e7bbebd5a3b21e188bee3360b69
Keerthivasan-s-j <jagankeerthisri@gmail.com>
2025-06-24 09:57:25 +0530

    2200

---
+++ Leetcode/2200.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<Integer> findKDistantIndices(int[] nums, int key, int k) {
+        List<Integer> res = new ArrayList<>();
+        List<Integer> lis = new ArrayList<>();
+        int n = nums.length;
+        for (int i = 0; i<n; i++) {
+            if (nums[i] == key)
+                lis.add(i);
+        }
+        for (int i = 0; i<n; i++) {
+            for (int j : lis) {
+                if (Math.abs(i-j) <= k) {
+                    res.add(i);
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+}
