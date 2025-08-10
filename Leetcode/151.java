65ff5561f0ca94bf3ea004fd00ebc88c79c38416
Keerthivasan-s-j <jagankeerthisri@gmail.com>
2025-08-10 23:06:49 +0530

    151

--- Leetcode/151.java
+++ Leetcode/151.java
@@ -1,33 +1,18 @@
+import java.util.*;
 class Solution {
     public String reverseWords(String s) {
-        s = s.trim();
-        int len = s.length();
-        String res = "";
-        String word = "";
-        for (int i = 0; i<len; i++){
-            char chr = s.charAt(i);
-            if (chr != ' '){
-                word += chr;
-            }
-            else if(chr == ' '){
-                if(res.length() != 0 && word.length() != 0){
-                    res = word + " " + res;
-                    word = "";
-                }
-                else{
-                    res = word + res;
-                    word = "";
-                }
+        String[] words = s.trim().split("\\s+");
+        Stack<String> stack = new Stack<>();
+        for (String word : words) {
+            stack.push(word);
+        }
+        StringBuilder res = new StringBuilder();
+        while (!stack.isEmpty()) {
+            res.append(stack.pop());
+            if (!stack.isEmpty()) {
+                res.append(" ");
             }
         }
-        if(res.length() != 0 && word.length() != 0){
-            res = word + " " + res;
-            word = "";
-        }
-        else{
-            res = word + res;
-            word = "";
-        }
-        return res;
+        return res.toString();
     }
 }
